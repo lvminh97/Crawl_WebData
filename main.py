14baf23666ff4f7c39162577a7bd9077f0724317
lvminh97 <lvminh97@gmail.com>
2022-03-04 17:52:20 +0700

    Export data to an Excel file
    Signed-off-by: lvminh97 <lvminh97@gmail.com>

--- main.py
+++ main.py
@@ -5,10 +5,12 @@
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from openpyxl import Workbook as WB
 from time import sleep
 
 __DRIVERPATH__      = "driver/chromedriver.exe"
 __URL__             = "https://dichvucong.quangninh.gov.vn/Default.aspx?tabid=119";
+__EXPORTFILE__      = "export/dichvucong.xlsx"
 
 browser = webdriver.Chrome(executable_path = __DRIVERPATH__)
 browser.get(__URL__)
@@ -41,7 +43,14 @@ for page in range(79):
     browser.find_element(by = By.CLASS_NAME, value = "rgPageNext").click()
     sleep(1)
 
-print(data)
+# Export data to Excel file
+output = WB()
+sheet = output.active
+for i in range(len(data)):
+    sheet.cell(row = i + 1, column = 1).value = data[i]['id']
+    sheet.cell(row = i + 1, column = 2).value = data[i]['cauhoi']
+    sheet.cell(row = i + 1, column = 3).value = data[i]['linhvuc']
+output.save(__EXPORTFILE__)
 
 # Close browser
 browser.quit()
